0d25acf75a09c2f7ad46b5a2aecefa404c15aeca
Isisl <isisl@sfu.ca>
2015-10-28 17:05:02 -0700

    updated files hopefully?

---
+++ Assets/camera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+
+	public Transform cam;
+	public float dist;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = cam.transform.position;
+		transform.rotation = cam.transform.rotation;
+		transform.Translate (Vector3.forward * -dist);
+		transform.Translate (Vector3.up * dist);
+		transform.LookAt (cam);
+	}
+}
+//Manshant 
---
+++ Assets/hehe.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hehe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+		transform.Translate (Vector3.forward*y);
+		transform.Translate (Vector3.right * x);
+	}
+}
